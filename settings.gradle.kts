d68fb400b46fd214db7039cb93f7e9b4ce75ecf2
Clay Johnson <cjohnson@gradle.com>
2026-05-20 13:59:06 -0500

    build: onboard to the OSS Community Develocity Instance (#35)

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,11 +1,32 @@
 plugins {
-    id("com.gradle.develocity") version("4.4.1")
+    id("com.gradle.develocity") version "4.4.1"
+    id("com.gradle.common-custom-user-data-gradle-plugin") version "2.6.0"
 }
 
+val isCI = System.getenv("CI") != null
+
 develocity {
+    server = "https://community.develocity.cloud"
+    projectId = "paweloczadly"
     buildScan {
-        termsOfUseUrl = "https://gradle.com/help/legal-terms-of-use"
-        termsOfUseAgree = "yes"
+        uploadInBackground = !isCI
+        publishing.onlyIf { it.isAuthenticated }
+        obfuscation {
+            ipAddresses { addresses -> addresses.map { _ -> "0.0.0.0" } }
+        }
+    }
+}
+
+buildCache {
+    local {
+        isEnabled = true
+    }
+
+    remote(develocity.buildCache) {
+        isEnabled = true
+        // Check access key presence to avoid build cache errors on PR builds when access key is not present
+        val accessKey = System.getenv("DEVELOCITY_ACCESS_KEY")
+        isPush = isCI && accessKey != null
     }
 }
 
